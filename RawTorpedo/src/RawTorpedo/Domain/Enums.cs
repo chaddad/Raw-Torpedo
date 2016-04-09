fb1889c610b33fa4b08a768c1b4cb8dabcf27c63
Chris Haddad <chris.haddad@gmail.com>
2016-04-08 22:20:02 -0400

    added enum file, and enums for average play times

--- RawTorpedo/src/RawTorpedo/Domain/Enums.cs
+++ RawTorpedo/src/RawTorpedo/Domain/Enums.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RawTorpedo.Domain
 {
@@ -9,12 +6,25 @@ namespace RawTorpedo.Domain
     {
         public enum Playtime
         {
+            [Display(Name = "Less than 15 minutes")]
             Quick,
+
+            [Display(Name = "15 to 30 minutes")]
             Short,
+
+            [Display(Name = "30 to 60 minutes")]
             Medium,
+
+            [Display(Name = "1 to 2 hours")]
             Long,
+
+            [Display(Name = "2 to 4 hours")]
             WickedLong,
+
+            [Display(Name = "4 to 8 hours")]
             AllDamnDay,
+
+            [Display(Name = "Over 8 hours")]
             SeriouslyTwoFuckingDays
         };
     }
