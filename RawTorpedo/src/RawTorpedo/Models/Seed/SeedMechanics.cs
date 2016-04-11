1823e4ba68006e671a8b6d9915b681803e14036c
Chris Haddad <chris.haddad@gmail.com>
2016-04-11 18:27:58 -0400

    added individual seed classes

---
+++ RawTorpedo/src/RawTorpedo/Models/Seed/SeedMechanics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RawTorpedo.Models.Seed
+{
+    public class SeedMechanics
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+
+        }
+    }
+}
